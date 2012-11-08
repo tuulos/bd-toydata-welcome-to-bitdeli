7960ee12514765e03c69ab8686b24e6c6cdf8be5
ville <ville@bitdeli.com>
2012-11-08 03:27:08 +0000

    vintage

--- __main__.py
+++ __main__.py
@@ -8,7 +8,7 @@ for profile in profiles():
         if event['event'] == 'buy':
             sales[event['product']] += event['price']
 
-set_theme('sail')
+set_theme('vintage')
 
 Text(head='Welcome to Bitdeli',
      size=(12,2),
