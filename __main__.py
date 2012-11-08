a845a80c34e37f4adc094e54a50aace08c1b995e
ville <ville@bitdeli.com>
2012-11-08 03:19:12 +0000

    testcommit 1

--- __main__.py
+++ __main__.py
@@ -8,7 +8,7 @@ for profile in profiles():
         if event['event'] == 'buy':
             sales[event['product']] += event['price']
 
-set_theme('lipstick')
+set_theme('purple')
 
 Text(head='Welcome to Bitdeli',
      size=(12,2),
